2c5cd164101fe8dde0ca8ae8e760587fbc53e433
majietank <majie.tank@Outlook.com>
2018-12-04 13:39:59 +0800

    201812041339

--- Study/main.c
+++ Study/main.c
@@ -3,6 +3,7 @@ int main()
 {
 	printf("Hello!\n");
 	printf("Hello!!\ttest\tdone\n");
-	printf("Github test,write!")
+	printf("Github test,write!");
+	printf("Visual Studio 2017,yee");
 	return 0;
 }
