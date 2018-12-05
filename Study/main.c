3a5d7ba2b7906132f8fb39b558ec592b5858c493
majietank <majie.tank@Outlook.com>
2018-12-05 15:46:14 +0800

    201812051546

--- Study/main.c
+++ Study/main.c
@@ -1,15 +1,34 @@
 #include<stdio.h>
 void sj()
 {
-	for(int a=0;a<=10;a++){
-		printf("***\n");
+	int high;
+	printf("输入高：");
+	scanf_s("%d",&high);
+	if(high%2==1){
+	for (int a = 0; a < high; a++) {
+		for (int b = 1; b < high*2; b++) {
+
+			if (b < high&&b + a < high || b >= high && b - a >= high + 1) {
+				printf(" ");
+			}
+			else
+			{				
+				printf("*");
+			}			
+		}
+		printf("\n");
 	}
-	
+	}
+	else
+	{
+		printf("error，偶数无法实现等腰三角形。\n");
+	}
 }
 int main()
 {
-
+	int a=11/2;
 	printf("Hello!\n");
+	printf("%d\n",a);
 	sj();
 	/*printf("Hello!!\ttest\tdone\n");
 	printf("Github test,write!");
